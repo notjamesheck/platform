89dfc9d83f5b0590a60af178dfa2edd608efca73
James Heck <224@holbertonschool.com>
2018-05-29 19:07:48 -0700

    Remove ssl from prod config

--- config/prod.exs
+++ config/prod.exs
@@ -16,8 +16,7 @@ use Mix.Config
 
 config :platform, PlatformWeb.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "https", host: "elixirpractice.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: "elixirpractice.herokuapp.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
--- config/prod.exs~
+++ config/prod.exs~
@@ -16,7 +16,8 @@ use Mix.Config
 
 config :platform, PlatformWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "elixirpractice.herokuapp.com", port: 80],
+  url: [scheme: "https", host: "elixirpractice.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
